87dc2ffcf748d34a35c43aad52d0279d521473f0
codeandrey@ukr.net <codeandrey@ukr.net>
2018-08-08 07:40:53 +0300

    add fonts

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+	$('.mobile__menu').click(function(){
+		var menu = document.getElementById('primary-menu'); 
+		if(menu.style.display == "block") {
+    		menu.style.display = "none";
+		} else { 
+		    menu.style.display = "block";
+		}
+	});
 
 	$(window).bind('scroll', function () {
 	    if ($(window).scrollTop() > 0) {
@@ -8,16 +16,15 @@ $(document).ready(function(){
 	    }
 	});
 
-  var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
-
-  
-
+  	var swiper = new Swiper('.swiper-container', {
+	    slidesPerView: 4,
+	    spaceBetween: 30,
+	    breakpoints: {
+		    767: {
+		      slidesPerView: 2,
+		      spaceBetween: 30
+		    }
+	  	}
+	});
 });
 
